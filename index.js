1d54d261999cbfc0e24f963011192ffdece7756f
Manish Pandey <manishpandey.rs.che23@itbhu.ac.in>
2024-08-27 18:24:23 +0530

    cors updated

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const productRouter = require('./route/product.route');
 
 
 
-app.use(cors({ origin: '*' }))
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // If you need to send cookies or authorization headers
+}))
 app.use(express.json())
 app.use('/user', userRouter)
 app.use('/product', authUserTask, productRouter)
